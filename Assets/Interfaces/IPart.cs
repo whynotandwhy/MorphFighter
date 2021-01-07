935651e7de2e59d0055b8e81fafd88da229d9f9e
Brian <whynotandwhy@gmail.com>
2021-01-07 10:07:19 -0500

    First Interface definition
    this was stored to preserve and push data before meeting, 
    
    IPart is a core interface for data

---
+++ Assets/Interfaces/IPart.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// IPart is the core defintion for items that are used to power the fighters
+/// </summary>
+public interface IPart
+{
+    /// <summary>
+    /// Focus is to represent a mental attribute for fighting
+    /// </summary>
+    float Focus { get; set; }
+    /// <summary>
+    /// Effort is to represent a physical attribute for fighting 
+    /// </summary>
+    float Effort { get; set; }
+    /// <summary>
+    /// Strength is to represent a secondary physical attribute for fighting 
+    /// </summary>
+    float Strength { get; set; }
+    /// <summary>
+    /// Rank is used to do quick comparisons and evaluations of objects
+    /// </summary>
+    int Rank { get; set; }
+}
