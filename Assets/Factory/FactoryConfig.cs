2a97232d0bb8e54118c000832c169d6dd562f3aa
Brian <whynotandwhy@gmail.com>
2021-01-09 06:36:55 -0500

    Unstable - Introduction of factory Concept
    A factory concept is being tried while this is a new project and details with come with testing.

---
+++ Assets/Factory/FactoryConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Factory
+{
+    [CreateAssetMenu(fileName = "FactoryInstance", menuName = "ScriptableObjects/FactoryConfig", order = 1)]
+    public class FactoryConfig : ScriptableObject
+    {
+        protected FactoryConfig instance;
+        public void Awake()
+        {
+            if (instance != default)
+                Debug.Log("Possible Multple FactoryCofigs found" + AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this)));
+            else
+                instance = this;
+        }
+        public static FactoryConfig Instance => Instance;
+
+        [SerializeField] protected ICoreFactory coreFactory;
+        public static ICoreFactory CoreFactory => Instance.coreFactory;
+    }
+}
---
+++ Assets/Interfaces/ICoreFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICoreFactory
+{
+    IAbility FindAbility(int ablityID);
+    IAbility FindAbility(string ablityName);
+    IAbilityEffect NewAbilityEffect(float Concentration, float Energy, float HP);
+    ICombatant NewCombatant(IEnumerable<IPart> components, IEnumerable<IAbility> abilites);
+    ICombatStats NewCombatStats();
+    ICombatStats NewCombatStats(ICombatStats source, IAbilityEffect abilityEffect);
+    IPart NewPart(float focus, float effort, float strength);
+}
