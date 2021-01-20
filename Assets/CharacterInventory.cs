fc4be30d67ecac899fd9a0f799921047202fdb2b
Brian Anderson <whynotandwhy@gmail.com>
2021-01-20 10:00:27 -0500

    preliminary stat display work
    requires discussion

--- Assets/CharacterInventory.cs
+++ Assets/CharacterInventory.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Factory;
 
 public class CharacterInventory : SlotGroup
 {
     protected CharacterStatUpdater characterStatUpdater;
+    protected ICombatant currentCombatant;
+    protected ICombatant predictedCombatant;
 
     public override bool NewInventoryRequest(IPart sourcePart)
     {
@@ -32,10 +35,24 @@ public class CharacterInventory : SlotGroup
         base.Awake();
     }
 
+    public void Start()
+    {
+         characterStatUpdater.UpdateUI(FactoryConfig.CoreFactory.NewCombatStats(.1f,.3f,.4f,.5f,1));
+    }
+
     protected override void AddItemToSlot(IPart part, ISlot slotDestination)
     {
         base.AddItemToSlot(part, slotDestination);
 
         // Here we need to update the character stats.
+
+        //TODO: there should be some assoation based in the slots themselves to identify which slock they are in.
+        List<KeyValuePair<int, IPart>> newitems = new List<KeyValuePair<int, IPart>>();
+        int index = 0;
+        foreach (ISlot partialList in this.Slots)
+            newitems.Add(new KeyValuePair<int, IPart>(index++, partialList.part));
+
+        currentCombatant.SetComponentParts(newitems);
+        characterStatUpdater.UpdateUI(currentCombatant.StartingCombats);
     }
 }
